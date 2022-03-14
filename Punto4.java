d24f1ed3691738531244378267ff2c557e1995a1
jhostia <101542114+jhostia@users.noreply.github.com>
2022-03-14 00:06:59 -0500

    Update Punto4.java

--- Punto4.java
+++ Punto4.java
@@ -1,16 +1,3 @@
-
-import java.util.Scanner;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Lenovo
- */
 public class Punto4 {
     public static void main (String[] args) {
         Scanner entrada = new Scanner (System.in);
