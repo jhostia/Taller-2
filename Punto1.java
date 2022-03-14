dc789c904138d70ec1ba41ba505eb663ba1cad9e
jhostia <101542114+jhostia@users.noreply.github.com>
2022-03-14 00:09:45 -0500

    Update Punto1.java

--- Punto1.java
+++ Punto1.java
@@ -1,25 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Lenovo
- */
-
-import java.util.Scanner;
-
-/**
- *
- * @author asus
- */
-
 public class Punto1 {
     
     public static void main(String[] args) { 
-        int x = 16;
+        int x = 8;
         
         System.out.printf("x = %d\n",x );
         System.out.printf("El valor de %d + %d es %d\n",x,x,(x + x) );
